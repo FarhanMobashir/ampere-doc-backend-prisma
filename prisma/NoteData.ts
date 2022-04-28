1ae50f8f526149cf2a422118c1ff2f3a265ff0fa
Mobashir Farhan <farhanmobashir007@gmail.com>
2022-04-29 02:08:58 +0530

    feat: init
    - adds the model and seeded the db with seed script

---
+++ prisma/NoteData.ts
@@ -0,0 +1,14 @@
+export const notes = [
+    {
+        title:"Note 1",
+    body:"Hello note 1 body"
+    },
+    {
+        title:"Note 2",
+    body:"Hello note 2 body"
+    },
+    {
+        title:"Note 3",
+    body:"Hello note 3 body"
+    }
+]
---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import bcryptjs from "bcryptjs";
+import { notes } from "./NoteData";
+
+const prisma = new PrismaClient();
+
+const run = async () => {
+  const salt = bcryptjs.genSaltSync(10);
+  const user = await prisma.user.upsert({
+    where: { email: "user@test.com" },
+    update: {},
+    create: {
+      email: "user@test.com",
+      password: bcryptjs.hashSync("password", salt),
+      firstName: "Scott",
+      lastName: "Moss",
+    },
+  });
+  await Promise.all(
+    notes.map(async (item) => {
+      return prisma.note.create({
+        data: {
+          title: item.title,
+          body: item.body,
+          author: {
+            connect: {
+              id: user.id,
+            },
+          },
+        },
+      });
+    })
+  );
+};
+
+run()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
